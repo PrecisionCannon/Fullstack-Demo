106b6e3f47219f18aa44a4c62bd179d671616ad8
PrecisionCannon <viviandean144@gmail.com>
2025-01-11 07:49:06 +0800

    Server project setup

---
+++ server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+
+namespace FullstackDemo;
+
+class Car{
+    public Car(string name, string make, DateTime expiry){
+        _name = name;
+        _make = make;
+        _expiry = expiry;
+    }
+    public string _name;
+    public string _make;
+    protected DateTime _expiry;
+    public bool checkExpiry(){
+        DateTime now = DateTime.Now;
+        return DateTime.Compare(_expiry, now) >= 0; 
+    }
+}
+
+class CarsService{
+    protected List<Car> cars;
+
+}
