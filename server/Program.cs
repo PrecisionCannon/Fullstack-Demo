a1aa7e539c669b7001922fb2bc5db2e0f2d89548
PrecisionCannon <viviandean144@gmail.com>
2025-01-11 08:07:15 +0800

    Cars deserialisation

--- server/Program.cs
+++ server/Program.cs
@@ -1,24 +1,28 @@
 ﻿using System;
-using System.Web;
+using System.Text.Json;
 
 namespace FullstackDemo;
 
 class Car{
+    public string _name;
+    public string _make;
+    protected DateTime _expiry;
     public Car(string name, string make, DateTime expiry){
         _name = name;
         _make = make;
         _expiry = expiry;
     }
-    public string _name;
-    public string _make;
-    protected DateTime _expiry;
-    public bool checkExpiry(){
+    public bool CheckExpiry(){
         DateTime now = DateTime.Now;
         return DateTime.Compare(_expiry, now) >= 0; 
     }
 }
 
 class CarsService{
-    protected List<Car> cars;
+    protected List<Car> _cars;
+    public CarsService(string carsLocation){
+        string carsJson = File.ReadAllText(carsLocation);
+        _cars = JsonSerializer.Deserialize<List<Car>>(carsJson) ?? throw new Exception("Invalid cars JSON file");
+    }
 
 }
